5606fdcf207f220c6b5bf34e1531dbc6c845d289
Joab Gonzalez <joab.gonzalez@tsoftlatam.com>
2018-02-27 17:08:45 -0300

    actualizando script de ejecucion

--- exec.sh
+++ exec.sh
@@ -9,15 +9,15 @@ while getopts r:v:t:s: option; do
 done
 if [ ${ROL} = "server" ]; then
     docker run -i -t \
-    -v ${VOLUME}/tests/data:/home/tsoft/apache-jmeter/bin/tsoft/tests/data \
-    -v ${VOLUME}/certificate:/home/tsoft/apache-jmeter/bin/tsoft/certificate \
+    -v ${VOLUME}/tests/data:/home/tsoft/apache-jmeter/bin/workspace/data \
+    -v ${VOLUME}/certificate:/home/tsoft/apache-jmeter/bin/workspace/certificate \
     -p 1099:1099 \
     --name jmeter-server --hostname jmeter-server --rm tsoft/jmeter-server
 elif [ ${ROL} = "master" ]; then
     docker run -i -t \
-    -v ${VOLUME}/tests:/home/tsoft/apache-jmeter/bin/tsoft/tests \
-    -v ${VOLUME}/results:/home/tsoft/apache-jmeter/bin/tsoft/results \
-    -v ${VOLUME}/certificate:/home/tsoft/apache-jmeter/bin/tsoft/certificate \
+    -v ${VOLUME}/tests:/home/tsoft/apache-jmeter/bin/workspace/tests \
+    -v ${VOLUME}/results:/home/tsoft/apache-jmeter/bin/workspace/results \
+    -v ${VOLUME}/certificate:/home/tsoft/apache-jmeter/bin/workspace/certificate \
     -e test_name=${TEST} -e ip_server=${SERVER} \
     --name jmeter-master --hostname jmeter-master --rm tsoft/jmeter-master
 fi
