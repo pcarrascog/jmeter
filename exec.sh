6f66377774ab5cc88d44ac679a11a15ff510986b
Joab Gonzalez <joab.gonzalez@tsoftlatam.com>
2018-02-27 16:27:33 -0300

    version definitiva

--- exec.sh
+++ exec.sh
@@ -10,13 +10,14 @@ done
 if [ ${ROL} = "server" ]; then
     docker run -i -t \
     -v ${VOLUME}/tests/data:/home/tsoft/apache-jmeter/bin/tsoft/tests/data \
+    -v ${VOLUME}/certificate:/home/tsoft/apache-jmeter/bin/tsoft/certificate \
     -p 1099:1099 \
     --name jmeter-server --hostname jmeter-server --rm tsoft/jmeter-server
 elif [ ${ROL} = "master" ]; then
     docker run -i -t \
     -v ${VOLUME}/tests:/home/tsoft/apache-jmeter/bin/tsoft/tests \
     -v ${VOLUME}/results:/home/tsoft/apache-jmeter/bin/tsoft/results \
+    -v ${VOLUME}/certificate:/home/tsoft/apache-jmeter/bin/tsoft/certificate \
     -e test_name=${TEST} -e ip_server=${SERVER} \
     --name jmeter-master --hostname jmeter-master --rm tsoft/jmeter-master
 fi
-#-v ${VOLUME}/certificate:/home/tsoft/apache-jmeter/bin/tsoft/certificate \
